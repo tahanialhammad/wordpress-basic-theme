e62f728245d3ea24dd95ba8c3a7dc56f91121b0b
tahanialhammad <tahani_alhammad@yahoo.com>
2024-09-23 13:11:42 +0200

    update

--- header.php
+++ header.php
@@ -92,6 +92,7 @@
                 )
               );
               ?>
+              
               <div class="py-6">
                 <a href="#" class="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Log in</a>
               </div>
